e444e39a4e8d26bb8c7cc391d06b04f083d95173
kheaji <114206084+kheaji@users.noreply.github.com>
2023-07-25 00:02:43 +0900

    data.php

--- data.php
+++ data.php
@@ -1,13 +1,18 @@
-<?php
-// 데이터베이스나 다른 소스로부터 옵션 데이터를 가져오는 로직을 구현
-// 이 예제에서는 간단히 배열로 제공합니다.
-$options = array(
-    array('value' => 'option1', 'label' => '옵션 1'),
-    array('value' => 'option2', 'label' => '옵션 2'),
-    array('value' => 'option3', 'label' => '옵션 3'),
-    // 추가 옵션들...
-);
-
-// JSON 형식으로 데이터 반환
-header('Content-Type: application/json');
-echo json_encode($options);
+{
+  "list": [
+    {
+      "key1": "value1",
+      "key2": "value1",
+      "key3": "value1",
+      "key4": "value1",
+      "key5": "value1"
+    },
+    {
+      "key1": "value2",
+      "key2": "value2",
+      "key3": "value2",
+      "key4": "value2",
+      "key5": "value2"
+    }
+  ]
+}
